d1bc363f3698ab97222603daaafff9e878b111d3
riki afifuddin <rikiafifuddin@gmail.com>
2022-08-24 22:35:28 +0700

    deploy

--- libs/db.js
+++ libs/db.js
@@ -1,7 +1,8 @@
 var knex = require('knex')({
   client: "mysql",
   connection: {
-    host: "34.101.166.97",
+    // host: "34.101.166.97",
+    socketPath: "/cloudsql/triples-359711:asia-southeast2:triples",
     user: "triples",
     password: "asusx450C#",
     database: 'fullstacknextjs'
