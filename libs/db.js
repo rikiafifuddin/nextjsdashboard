64dcb2cdcf190507018954258ce67520b5da6c46
riki afifuddin <rikiafifuddin@gmail.com>
2022-08-24 21:48:21 +0700

    tyr using hardcoded

--- libs/db.js
+++ libs/db.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
-  client: process.env.DB_CLIENT,
+  client: "mysql",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: "34.101.166.97",
+    user: "triples",
+    password: "asusx450C#",
+    database: 'fullstacknextjs'
   }
 })
 
