687d45ebd2cb60ba5b12660582b085936bbbd3a7
riki afifuddin <rikiafifuddin@gmail.com>
2022-09-11 21:23:45 +0700

    fix connection

--- libs/db.js
+++ libs/db.js
@@ -1,9 +1,9 @@
 var knex = require('knex')({
   client: "mysql",
   connection: {
-    host: "34.101.166.97",
+    // host: "34.101.166.97",
 
-    // socketPath: "/cloudsql/triples-359711:asia-southeast2:triples",
+    socketPath: "/cloudsql/triples-359711:asia-southeast2:triples",
     user: "triples",
     password: "asusx450C#",
     database: 'fullstacknextjs'
